9442e5670d43ce9a5618472e35cb44af1191ded3
vektorver <vektorver@gmail.com>
2022-10-17 14:04:36 +0400

    start commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# python script
+
+import os
+
+config = {'obj.data': 'obj.data', 
+       'cfg':'yolov4-obj.cfg',
+       'weights': 'yolov4-obj_best.weights', 
+       'thresh': 0.9}
+
+class ObDetModel:
+  
+  def __init__(self, config):
+    self.obj_data = config['obj.data']
+    self.cfg = config['cfg']
+    self.weights = config['weights']
+    self.thresh = config['thresh']
+
+  def predict(self, path):
+    cmd = './darknet detector test {} {} {} {} -thresh {}'.format(self.obj_data, 
+                                                                  self.cfg, 
+                                                                  self.weights, 
+                                                                  path,
+                                                                  self.thresh)
+    so = os.popen(cmd).read()
+
+    return 'Result was saved to predictions.jpg'
+
+model = ObDetModel(config)
+model.predict('1657.jpg')
